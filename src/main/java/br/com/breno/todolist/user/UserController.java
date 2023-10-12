022024ab5450279b26075778affae16362afd618
felipsbreno <brendo.filipe2050@gmail.com>
2023-10-12 15:26:59 -0300

    add bcrypt to create and save a password

--- src/main/java/br/com/breno/todolist/user/UserController.java
+++ src/main/java/br/com/breno/todolist/user/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -29,6 +31,9 @@ public class UserController {
     if (username != null) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário com esse username já está em uso!");
     }
+
+    var passwordHash = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+    userModel.setPassword(passwordHash.toString());
 
     var userCreate = this.userRepository.save(userModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreate);
