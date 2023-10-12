75f1ef27ba04c98069df546df5cdaf0efd038b33
felipsbreno <brendo.filipe2050@gmail.com>
2023-10-12 11:14:34 -0300

    add JPA in model and the annotations

--- src/main/java/br/com/breno/todolist/user/UserModel.java
+++ src/main/java/br/com/breno/todolist/user/UserModel.java
@@ -1,14 +1,26 @@
 package br.com.breno.todolist.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
-@Getter
-@Setter
-@AllArgsConstructor
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
-  public String username;
-  public String name;
-  public String email;
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+
+  private String username;
+  private String name;
+  private String email;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
