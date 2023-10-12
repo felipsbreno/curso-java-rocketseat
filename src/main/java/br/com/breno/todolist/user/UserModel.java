d9f10a561831e95ee882b494d1b5f110c750f713
felipsbreno <brendo.filipe2050@gmail.com>
2023-10-12 09:59:51 -0300

    add lombok and see how using getter and setter

--- src/main/java/br/com/breno/todolist/user/UserModel.java
+++ src/main/java/br/com/breno/todolist/user/UserModel.java
@@ -1,5 +1,12 @@
 package br.com.breno.todolist.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class UserModel {
   public String username;
   public String name;
